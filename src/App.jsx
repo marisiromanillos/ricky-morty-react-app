acc1af1614e9c715902fec2d6fd65faf4e2b827d
Marisi Romanillos <marisiromanillos@Marisis-MBP.lan>
2023-06-15 11:41:48 +0100

    axios working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import axios from "axios";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
 
 const App = () => {
+  let [pageNumber, setPageNumber] = useState(1);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(api);
+      console.log(data.results);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="ext-start ubuntu">
+      <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">WiKi</span>
       </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
